c11143b2b333684b372d1e2cf3f42421765008bd
liewteh <73503571+liewteh@users.noreply.github.com>
2021-06-13 14:52:26 +0100

    Initialize postgres

---
+++ server/db.js
@@ -0,0 +1,24 @@
+import { Pool } from "pg";
+
+const dbUrl = process.env.DATABASE_URL || "postgres://localhost:5432/cyf";
+
+const pool = new Pool({
+	connectionString: dbUrl,
+	connectionTimeoutMillis: 5000,
+});
+
+export const connectDb = async () => {
+	let client;
+	try {
+		client = await pool.connect();
+	} catch (err) {
+		console.error(err);
+		process.exit(1);
+	}
+	console.log("Postgres connected to", client.database);
+	client.release();
+};
+
+export const disconnectDb = () => pool.close();
+
+export default { query: pool.query };
